4dc5d8da1148f073094f8559eff596fcac70438e
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2026-06-25 18:52:55 +0530

    Time: 533 ms (55.22%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,10 @@
+SELECT
+    p.project_id , ROUND(AVG(e.experience_years) ,2) as average_years 
+FROM 
+    Project p 
+join
+    Employee e
+on 
+    p.employee_id  = e.employee_id 
+group by
+     p.project_id  ;
