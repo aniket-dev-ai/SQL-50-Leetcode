4c9fe09eadbf80ca6a816f356f8361bf38156d1e
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2026-06-26 21:34:44 +0530

    Time: 373 ms (61.04%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,14 +1,9 @@
-select query_name,
-ROUND(
-    avg(
-        rating / position) , 2) as quality ,
-Round(
-        (
-            select count(rating) 
-            from Queries where rating < 3 
-            and q.query_name = query_name
-        )
-        /count(rating) * 100 , 2)
-         as poor_query_percentage 
-from Queries q
-group by query_name;
+SELECT 
+query_name , ROUND(AVG(rating/position),2) as quality,
+IFNULL(Round((
+    select count(*) from Queries q2 where q1.query_name = q2.query_name and q2.rating < 3
+    order by query_name
+)/count(q1.rating) * 100 , 2),0) as poor_query_percentage 
+from 
+Queries q1
+GROUP BY query_name;
