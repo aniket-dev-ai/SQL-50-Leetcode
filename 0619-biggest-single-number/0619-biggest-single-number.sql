875e7ffc6e30a43550a3daaff2e10c8368c272e9
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2026-06-26 16:59:56 +0530

    Time: 500 ms (16.18%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,7 +1,8 @@
-select max(num) as num
-from (
-    select num 
-    from MyNumbers 
-    group by num 
-    having count(num)=1
-) an
+# Write your MySQL query statement belo
+select case when count(num)=1 then num else null end as num
+from MyNumbers
+group by num  
+order by num desc
+limit 1
+
+ 
