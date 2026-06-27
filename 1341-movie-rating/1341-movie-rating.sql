5196b056c2a60a6973e714a1c4c11f953344f3ac
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2026-06-27 16:31:46 +0530

    Time: 1882 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,30 @@
+SELECT title AS results
+FROM
+(
+    (
+        SELECT
+            (SELECT name
+             FROM Users
+             WHERE Users.user_id = MovieRating.user_id) AS title,
+            COUNT(*) AS r
+        FROM MovieRating
+        GROUP BY user_id
+        ORDER BY r DESC, title
+        LIMIT 1
+    )
+
+    UNION
+
+    (
+        SELECT
+            (SELECT title
+             FROM Movies
+             WHERE Movies.movie_id = MovieRating.movie_id) AS title,
+            AVG(rating) AS avg_rating
+        FROM MovieRating
+        WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
+        GROUP BY movie_id
+        ORDER BY avg_rating DESC, title
+        LIMIT 1
+    )
+) AS r;
